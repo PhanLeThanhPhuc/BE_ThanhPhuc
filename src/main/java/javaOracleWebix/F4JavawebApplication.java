9d21b423b875155b8910485ce2ed955237f88884
PLTPhuc <pltp2010@gmail.com>
2022-12-18 18:15:44 +0700

    commit 1

---
+++ src/main/java/javaOracleWebix/F4JavawebApplication.java
@@ -0,0 +1,13 @@
+package javaOracleWebix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F4JavawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F4JavawebApplication.class, args);
+	}
+
+}
