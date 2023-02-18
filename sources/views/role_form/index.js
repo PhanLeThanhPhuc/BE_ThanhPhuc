428cddf67f3e249baaeb74db4a382dbb31cb0f87
PLTPhuc <117081695+PhanLeThanhPhuc@users.noreply.github.com>
2023-02-18 08:14:08 +0700

    Update index.js

--- sources/views/role_form/index.js
+++ sources/views/role_form/index.js
@@ -54,7 +54,7 @@ export default class RoleForm extends JetView {
                 },
             ]
         }
-        
+        ////// check pull soucre
     }
     init() {
         service.onload();
